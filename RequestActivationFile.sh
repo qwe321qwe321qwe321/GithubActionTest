253afffca2a2b076857929b2d11d1a1b069e53fc
qwe321qwe321qwe321 <aegis321123@gamil.com>
2020-02-12 04:20:51 +0800

    fix

--- RequestActivationFile.sh
+++ RequestActivationFile.sh
@@ -1,4 +1,4 @@
 curl -X POST https://api.github.com/repos/qwe321qwe321qwe321/GithubActionTest/dispatches \
 -H 'Accept: application/vnd.github.everest-preview+json' \
--H 'Authorization: token 8007176b3b8590f4f595bfb9bde9a4d9b905b0c5' \
+-H 'Authorization: token 09f264467caf3ad1cc24c949295eccab8bfc69b0' \
 --data '{"event_type": "request_activation_file"}'
