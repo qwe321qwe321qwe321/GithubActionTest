806aebfb966642d4582a8a837c7385b6e98cc851
qwe321qwe321qwe321 <aegis321123@gamil.com>
2020-02-26 16:30:21 +0800

    modify if statement

--- RequestActivationFile.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST https://api.github.com/repos/qwe321qwe321qwe321/GithubActionTest/dispatches \
--H 'Accept: application/vnd.github.everest-preview+json' \
--H 'Authorization: token 09f264467caf3ad1cc24c949295eccab8bfc69b0' \
---data '{"event_type": "request_activation_file"}'
